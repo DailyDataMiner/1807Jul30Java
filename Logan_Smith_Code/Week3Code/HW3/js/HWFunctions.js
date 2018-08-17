24bfe41c2914c76e0c1e16beac5925d61f74adbd
Logan Smith <lorgansmith@gmail.com>
2018-08-17 01:07:13 -0400

    Working on hw3

--- Logan_Smith_Code/Week3Code/HW3/js/HWFunctions.js
+++ Logan_Smith_Code/Week3Code/HW3/js/HWFunctions.js
@@ -1,11 +1,11 @@
-var bubbleSort = function(numArray) {
-    for (var i = 0; i <= numArray.length-1; i++) {
+var bubbleSort = function (numArray) {
+    for (var i = 0; i <= numArray.length - 1; i++) {
         var finished = true;
-        for (var j = 1; j <= numArray.length-1; j++) {
-            if (numArray[j-1] > numArray[j]) {
+        for (var j = 1; j <= numArray.length - 1; j++) {
+            if (numArray[j - 1] > numArray[j]) {
                 var placeholder = numArray[j];
-                numArray[j] = numArray[j-1];
-                numArray[j-1] = placeholder;
+                numArray[j] = numArray[j - 1];
+                numArray[j - 1] = placeholder;
                 finished = false;
             }
         }
@@ -16,27 +16,115 @@ var bubbleSort = function(numArray) {
     return numArray;
 }
 
-var reverseStr = function(someStr) {
-    for (i = someStr.length-2; i >= 0; i--) {
-        someStr = someStr.substring(0, i) + someStr.substring(i+1, someStr.length) + someStr.substring(i, i+1);
+var reverseStr = function (someStr) {
+    for (i = someStr.length - 2; i >= 0; i--) {
+        someStr = someStr.substring(0, i) + someStr.substring(i + 1, someStr.length) + someStr.substring(i, i + 1);
     }
     return someStr;
 }
 
-var factorial = function(someNum) {
+var factorial = function (someNum) {
     if (someNum == 0) {
         return 1;
     }
     else {
-        return someNum*factorial(someNum-1);
+        return someNum * factorial(someNum - 1);
     }
 }
-/*
-public int factorialCalculator(int size) {
-		int output = 1; // Final output
-		for (int i = 1; i <= size; i++) { // Loops through the size
-			output = output*i; // output is multiplied
-		}
-		return output; // returns output
-	}
-*/
+
+var substring = function (someStr, length, offset) {
+    if ((typeof someStr) != "string") {
+        alert("You did not enter a string.");
+        return null;
+    }
+    if ((typeof length) != "number") {
+        alert("You did not enter a number for the length!");
+    }
+    if ((typeof offset) != "number") {
+        alert("You did not enter a number for the offset!");
+    }
+    return someStr.substring(offset, offset + length + 1);
+}
+
+var isEven = function (someNum) {
+    let checker = someNum;
+    someNum = parseInt(someNum / 2);
+    someNum = someNum * 2;
+    if (someNum == checker) {
+        return true;
+    }
+    return false;
+}
+
+var isPalindrome = function (someStr) {
+    let isPalindrome = true;
+    someStr = someStr.toLowerCase();
+    for (let i = 0; i < (someStr.length / 2); i++) {
+        if (someStr.charAt(i) != someStr.charAt(someStr.length - 1 - i)) {
+            isPalindrome = false;
+        }
+    }
+    return isPalindrome;
+}
+
+var printShape = function (shape, height, character) {
+    let outputLine = "";
+    switch (shape) {
+        case "Square":
+            for (let i = 0; i < height; i++) {
+                for (let j = 0; j < height; j++) {
+                    outputLine += character;
+                }
+                outputLine += "\n";
+            }
+            break;
+        case "Triangle":
+            for (let i = 0; i < height; i++) {
+                for (let j = i; j >= 0; j--) {
+                    outputLine += character;
+                }
+                outputLine += "\n";
+            }
+            break;
+        case "Diamond":
+            for (let j = 1; j <= height; j += 2) {
+                for (let k = 0; k < (height - j / 2 - height / 2); k++) {
+                    outputLine += " ";
+                }
+                for (let i = 1; i <= j; i++) {
+                    outputLine += character;
+                }
+                outputLine += "\n";
+            }
+            for (let j = height - 2; j >= 0; j -= 2) {
+                for (let k = 0; k < (height - j / 2 - height / 2); k++) {
+                    outputLine += " ";
+                }
+                for (let i = 1; i <= j; i++) {
+                    outputLine += character;
+                }
+                outputLine += "\n";
+            }
+            break;
+    }
+    console.log(outputLine);
+}
+
+var pug = {
+    name: "Tori",
+    size: "Medium",
+    age: 10,
+    powerLevel: 9000
+}
+
+var traceObj = function(someObj) {
+    for (var a in someObj) {
+        console.log(a + ": " + someObj[a]);
+    }
+}
+
+var deleteElement = function(someArray) {
+    console.log(someArray.length);
+    delete someArray[3];
+    console.log(someArray.length);
+}
